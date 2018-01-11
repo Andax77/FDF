90f629eacc5f08c7027b35004822f7b95cede81c
Andrew HUANG <anhuang@e2r6p15.42.fr>
2018-01-11 13:26:50 +0100

    MAJ

--- src/ft_trace.c
+++ src/ft_trace.c
@@ -35,11 +35,11 @@ void    ft_trace_line(t_point **tab, int height, int width, t_e *e)
     {
       if (x + 1 < width)
       {
-          ft_line(e->img_str, e->color, tab[y][x], tab[y][x + 1]);
+          ft_line(e->img_str, e->clr, tab[y][x], tab[y][x + 1]);
       }
       if (y + 1 < height)
       {
-          ft_line(e->img_str, e->color, tab[y][x], tab[y + 1][x]);
+          ft_line(e->img_str, e->clr, tab[y][x], tab[y + 1][x]);
       }
       x++;
     }
@@ -52,15 +52,9 @@ void    ft_trace(int **old, t_e *e, t_point size)
 {
   t_point pt;
   t_point **new;
+  double  z;
 
-  // for (int y = 0; y < size.y; ++y)
-  // {
-      // for (int x = 0; x < size.x; ++x)
-      // {
-        // printf("%d ", old[y][x]);
-//      }
-      // printf("\n");
-  // }
+  z = 3;
   if (!(new = (t_point**)malloc(sizeof(t_point*) * size.y)))
     exit(0);
   pt.y = 0;
@@ -71,8 +65,8 @@ void    ft_trace(int **old, t_e *e, t_point size)
     pt.x = 0;
     while (pt.x < size.x)
     {
-      new[pt.y][pt.x].x =  2 * (pt.x - pt.y) * (WIN_WIDTH / (3 * size.x)) + e->offset.x;
-      new[pt.y][pt.x].y = (pt.x + pt.y) * (WIN_WIDTH / (3 * size.x)) + e->offset.y;
+      new[pt.y][pt.x].x =  2 * (pt.x - pt.y) * e->zoom + e->offset.x;
+      new[pt.y][pt.x].y = (pt.x + pt.y) * e->zoom + e->offset.y;
       new[pt.y][pt.x].y += old[size.y - pt.y - 1][pt.x] * 2;
       ++pt.x;
     }
@@ -86,7 +80,7 @@ void    ft_trace(int **old, t_e *e, t_point size)
     // }
     // printf("\n");
   // }
-  printf("COULEUR : %d\n", e->color);
+  printf("COULEUR : %d\n", e->clr);
   ft_trace_line(new, size.y, size.x, e);
   free(new);
 }
